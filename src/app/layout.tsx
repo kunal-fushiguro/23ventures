89c33a1fe84836fede1dd34406941fe03b78afc6
kunal <natwarlaluzumaki@gmail.com>
2025-01-18 21:14:15 +0530

    finish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
-import ParticleBackground from "@/components/Background";
+// import ParticleBackground from "@/components/Background";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
